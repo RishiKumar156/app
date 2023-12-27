4df9913579132643c39a2512825b7be5d6366205
Pharaoh_devil <rishikk156@gmail.com>
2023-12-27 21:31:39 +0530

    updates

---
+++ backend/sources/routes/ds.js
@@ -0,0 +1,30 @@
+// import mydynamic from './mydynamicomponent';
+function read_the_api(apis)
+{
+    // going to read the apis 
+    // const myapis = apis 
+    /*
+        const mylinkedlist = linkedlist();
+        for (let i = 0; i < apis.length; i++)
+        {
+            mylinkedlist.push(apis[i]);
+        }
+    */
+}
+
+/*
+    {
+        mylinkedlist.map((nodes) => {
+            return (
+                <div>
+                    {nodes.somevalues}
+                </div>
+            )
+        })
+    }
+
+    Here insted of using array I have used the linked list right so what's the benifit of using it here can we improve the performance
+    of rendering item in the frontend more efficiently
+
+    Note: this is just an assumption not an actual code 
+*/
