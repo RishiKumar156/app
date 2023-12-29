a6b2cb9fc332c5c2d661a3c0b2040b6db63d7f0d
Pharaoh_devil <rishikk156@gmail.com>
2023-12-29 18:30:47 +0530

    test 2

--- frontend/site/src/mytest.py
+++ frontend/site/src/mytest.py
@@ -1,7 +1,9 @@
-from itertools import permutations
-def isGood(nums):
-    k = permutations(nums)
-    j = [i for i in k]
-    return j 
+def reverseVowels(s: str) -> str:
+    j = [i for i in s if i in 'aeiouAEIUO']
+    s = list(s)
+    for i in range(len(s)):
+        if s[i] in 'aeiouAEIOU':
+            s[i] = j.pop() 
+    return ''.join(s)
 
-print(isGood([1,2,3,3]))
+print(reverseVowels('leetcode'))
