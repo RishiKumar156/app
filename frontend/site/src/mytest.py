9c3929c6804768341cff47d6f4ab5acba28120fc
Pharaoh_devil <rishikk156@gmail.com>
2023-12-29 21:29:20 +0530

    min updations

--- frontend/site/src/mytest.py
+++ frontend/site/src/mytest.py
@@ -6,4 +6,20 @@ def reverseVowels(s: str) -> str:
             s[i] = j.pop() 
     return ''.join(s)
 
-print(reverseVowels('leetcode'))
+# print(reverseVowels('leetcode'))
+
+
+def firstUniqChar(s: str) -> int:
+    seen = []
+    idx = {}
+    for i in range(len(s)):
+        if s[i] not in seen:
+            seen.append(s[i])
+            idx[s[i]] = i
+        elif s[i] in seen:
+            idx.pop(s[i], None)
+    result = min(idx.values(), default=-1)
+    return result
+
+
+print(firstUniqChar("loveleetcode"))
